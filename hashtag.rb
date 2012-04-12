c7680bbb1ecb4395bb1959ef5c28366207b92881
Thomas Pun <thomas@nowbox.com>
2012-04-11 20:54:20 -0700

    clean up

--- hashtag.rb
+++ hashtag.rb
@@ -3,11 +3,10 @@ class HashTag < String
 
   def words
     matched = reverse_match
-    return [self] if self==matched || matched.nil?
+    return [self] if matched.nil? || self==matched
     [self[0..(length-1-matched.length)].words, matched].flatten
   end
 
-  # Find the longest matching word starting from the end
   def reverse_match
     length.times do |index|
       return self[index..-1] if Dictionary.include? self[index..-1]
