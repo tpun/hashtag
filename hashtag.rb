d02cf9920194f00441ce477549999435730d5468
Thomas Pun <thomas@nowbox.com>
2012-04-11 21:18:00 -0700

    more appropriate variable and method name

--- hashtag.rb
+++ hashtag.rb
@@ -2,12 +2,13 @@ class HashTag < String
   Dictionary = %w( i so like bad things and it is a true thing )
 
   def words
-    matched = reverse_match
-    return [self] if matched.nil? || self==matched
-    [self[0..(length-1-matched.length)].words, matched].flatten
+    word = longest_word_from_end
+    return [self] if word.nil? || self==word
+    [self[0..(length-1-word.length)].words, word].flatten
   end
 
-  def reverse_match
+private
+  def longest_word_from_end
     length.times do |index|
       return self[index..-1] if Dictionary.include? self[index..-1]
     end
