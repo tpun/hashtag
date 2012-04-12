1be6dcc28b2698a8263f713b04916a8403207a98
Thomas Pun <thomas@nowbox.com>
2012-04-11 20:27:04 -0700

    #words by using recursion

--- hashtag.rb
+++ hashtag.rb
@@ -1,5 +1,17 @@
 class HashTag < String
+  Dictionary = %w( i so like bad things and it is a true thing )
+
   def words
-    [self]
+    matched = reverse_match
+    return [self] if self==matched || matched.nil?
+    [self[0..(length-1-matched.length)].words, matched].flatten
+  end
+
+  # Find the longest matching word starting from the end
+  def reverse_match
+    length.times do |index|
+      return self[index..-1] if Dictionary.include? self[index..-1]
+    end
+    nil
   end
 end
--- hashtag_spec.rb
+++ hashtag_spec.rb
@@ -3,15 +3,20 @@ require './hashtag'
 describe HashTag do
   describe "#words" do
     let(:expected) { ['things', 'i', 'like']}
-    it "returns array of proper words"
+    it "returns array of proper words" do
+      HashTag.new(expected.join).words.should == expected
+    end
 
     let(:tricky) { ['thing', 'so', 'true', 'is', 'it', 'i'] }
-    it "handles overlapping words"
+    it "handles overlapping words" do
+      HashTag.new(tricky.join).words.should == tricky
+    end
 
     let(:random) { ["badtaaaag"] }
-    it "returns untouched string if there is any non proper word"
+    it "returns untouched string if there is any non proper word" do
+      HashTag.new(random.join).words.should == random
+    end
 
-    let(:camel_case) { "tRuethIngs" }
     it "ignores case"
   end
 end
